eba3912fb7581dfaca3870f0753f17412cfddda5
aishwarya <ashp17star@gmail.com>
2026-09-24 14:58:16 +0530

    Two Table Views inside Container views hosted on View controller and using custom cell and model classes.

---
+++ 22_07_2026_ContainerViewWithTableViewController/MainCourseItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainCourseItemTableViewCell.swift
+//  22_07_2026_ContainerViewWithTableViewController
+//
+//  Created by Vishal Jagtap on 24/09/26.
+//
+
+import UIKit
+
+class MainCourseItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 22_07_2026_ContainerViewWithTableViewController/SnackItem.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  22_07_2026_ContainerViewWithTableViewController
+//
+//  Created by Vishal Jagtap on 24/09/26.
+//
+
+import Foundation
